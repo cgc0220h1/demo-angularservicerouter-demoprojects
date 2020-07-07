80abf5de17035b1096a6b37f421beac0e771b7c9
Duc <vanduc2514@gmail.com>
2020-07-07 21:57:26 +0700

    Provide Template for Timelines Component

--- src/app/time-lived/timelines/timelines.component.ts
+++ src/app/time-lived/timelines/timelines.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-timelines',
   templateUrl: './timelines.component.html',
 })
 export class TimelinesComponent implements OnInit {
+  output: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onChange(value: string): void {
+  }
 }
