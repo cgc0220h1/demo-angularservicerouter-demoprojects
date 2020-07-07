69862abc0be0064c82323c60cf4d0bba0102733a
Duc <vanduc2514@gmail.com>
2020-07-07 22:15:41 +0700

    Add Date Util Service Add Logic for TimeLines Component

--- src/app/time-lived/timelines/timelines.component.ts
+++ src/app/time-lived/timelines/timelines.component.ts
@@ -1,18 +1,20 @@
 import {Component, OnInit} from '@angular/core';
+import {DateUtilService} from '../../services/date-util.service';
 
 @Component({
   selector: 'app-timelines',
   templateUrl: './timelines.component.html',
 })
 export class TimelinesComponent implements OnInit {
-  output: any;
+  output = '';
 
-  constructor() {
+  constructor(private dateUtilService: DateUtilService) {
   }
 
   ngOnInit(): void {
   }
 
   onChange(value: string): void {
+    this.output = this.dateUtilService.getDiffToNow(value);
   }
 }
